3521accc4bf200b079fbc61365d5f6f79c4e8a7f
PotatoPC <mark.esplana514@gmail.com>
2025-06-12 12:15:13 +0800

    Refined vpc and added a working cluster.yaml for eks

--- Infra2/modules/vpc/outputs.tf
+++ Infra2/modules/vpc/outputs.tf
@@ -1,24 +1,24 @@
 output "vpc_id" {
   description = "ID of the VPC"
-  value       = module.vpc.vpc_id
+  value       = aws_vpc.main.id
 }
 
 output "public_subnet_1_id" {
   description = "ID of public subnet 1"
-  value       = module.vpc.public_subnet_1_id
+  value       = aws_subnet.public_1.id
 }
 
 output "public_subnet_2_id" {
   description = "ID of public subnet 2"
-  value       = module.vpc.public_subnet_2_id
+  value       = aws_subnet.public_2.id
 }
 
 output "private_subnet_1_id" {
   description = "ID of private subnet 1"
-  value       = module.vpc.private_subnet_1_id
+  value       = aws_subnet.private_1.id
 }
 
 output "private_subnet_2_id" {
   description = "ID of private subnet 2"
-  value       = module.vpc.private_subnet_2_id
+  value       = aws_subnet.private_2.id
 }
